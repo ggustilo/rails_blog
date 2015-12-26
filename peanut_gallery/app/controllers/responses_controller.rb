57ab585510816f5e3c4429fb39891a75670b4c36
ggustilo <beastifer@gmail.com>
2015-12-26 12:26:41 -0500

    Tested routes with views - working

---
+++ peanut_gallery/app/controllers/responses_controller.rb
@@ -0,0 +1,26 @@
+class ResponsesController < ApplicationController
+
+	def new
+	end
+
+	def create
+	end
+
+	def index
+		render "_index"
+	end
+
+	def approve
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+
+end
--- peanut_gallery/config/routes.rb
+++ peanut_gallery/config/routes.rb
@@ -1,39 +1,40 @@
 Rails.application.routes.draw do
 # get "/posts/:id" => "posts#show"
 
-get "/" => "posts#index"
+  get "/" => "posts#index"
 
-get "/login" => "writers#login_form"
-post "/login" => "writers#login"
-delete "/logout" => "writers#logout"
+  get "/login" => "writers#login_form"
+  post "/login" => "writers#login"
+  delete "/logout" => "writers#logout"
 
-get "/register" => "writers#new"
-post "/register" => "writers#create"
+  get "/register" => "writers#new"
+  post "/register" => "writers#create"
 
-get "/writers/:id" => "writers#show"
-get "/writers/:id/edit" => "writers#edit"
-patch "/writers/:id" => "writers#update"
-put "/writers/:id" => "writers#update"
-delete "/writers/:id" => "writers#destroy"
+  get "/writers/:id" => "writers#show"
+  get "/writers/:id/edit" => "writers#edit"
+  patch "/writers/:id" => "writers#update"
+  put "/writers/:id" => "writers#update"
+  delete "/writers/:id" => "writers#destroy"
 
-get "/posts/new" => "posts#new"
-post "/posts" => "posts#create"
-post "/posts/:id/publish" => "posts#publish"
-get "/posts/:id/edit" => "posts#edit"
-patch "/posts/:id" => "posts#update"
-put "/posts/:id" => "posts#update"
-delete "/posts/:id" => "posts#destroy"
+  get "/posts/new" => "posts#new"
+  post "/posts" => "posts#create"
+  get "/posts/:id" => "posts#show"
+  post "/posts/:id/publish" => "posts#publish"
+  get "/posts/:id/edit" => "posts#edit"
+  patch "/posts/:id" => "posts#update"
+  put "/posts/:id" => "posts#update"
+  delete "/posts/:id" => "posts#destroy"
 
-get "/posts/:id/responses/new" => "posts/responses#new"
-post "/posts/:id/responses" => "posts/responses#create"
-get "/posts/:id/responses/:id/edit" => "posts/responses#edit"
-patch "/posts/:id/responses/:id" => "posts/responses#update"
-put "/posts/:id/responses/:id" => "posts/responses#update"
-delete "/posts/:id/responses/:id" => "posts/responses#destroy"
+  get "/posts/:id/responses/new" => "responses#new"
+  post "/posts/:id/responses" => "responses#create"
+  get "/posts/:id/responses/:id/edit" => "responses#edit"
+  patch "/posts/:id/responses/:id" => "responses#update"
+  put "/posts/:id/responses/:id" => "responses#update"
+  delete "/posts/:id/responses/:id" => "responses#destroy"
 
-get "/writers/:id/posts/:id/responses" => "posts/responses#index"
-post "/writers/:id/posts/:id/responses/:id" => "posts/responses#approve"
-# TODO - add ability to include only three approved responses with original post
+  get "/writers/:id/posts/:id/responses" => "responses#index"
+  post "/writers/:id/posts/:id/responses/:id" => "responses#approve"
+  # TODO - add ability to include only three approved responses with original post
 
 end
 
