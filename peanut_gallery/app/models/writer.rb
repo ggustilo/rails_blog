8c705341b0292f775fce58de7b81cb355d6d4fea
ggustilo <beastifer@gmail.com>
2015-12-26 10:16:25 -0500

    Fix writer associations

--- peanut_gallery/app/models/writer.rb
+++ peanut_gallery/app/models/writer.rb
@@ -1,13 +1,10 @@
-# require 'bcrypt'
-
 class Writer < ActiveRecord::Base
 
 	include BCrypt
 
 	has_many :posts
-	has_many :responses
-	has_many :post_responses, through: :posts, source: :responses
-	has_many :responders, through: :post_responses, source: :writer
+  has_many :responses
+  has_many :responders, through: :responses, source: :writer
 
 	def password
     @password ||= Password.new(password_hash)
