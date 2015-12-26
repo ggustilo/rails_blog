48cba29f278b868fbf6cb2da101cf566d0c47d6d
ggustilo <beastifer@gmail.com>
2015-12-26 10:40:49 -0500

    Tested rails associations in console; working; plus add bcrypt

--- peanut_gallery/app/models/writer.rb
+++ peanut_gallery/app/models/writer.rb
@@ -4,7 +4,8 @@ class Writer < ActiveRecord::Base
 
 	has_many :posts
   has_many :responses
-  has_many :responders, through: :responses, source: :writer
+  has_many :post_responses, through: :posts, source: :responses
+  has_many :responders, through: :post_responses, source: :writer
 
 	def password
     @password ||= Password.new(password_hash)
